4d05b2503250cdab6a5e3a813cee777b8163564d
Chloe Page <chloesapagepersonal@gmail.com>
2025-09-19 17:49:12 +0100

    chore: remove unneeded imports

--- app/src/main/java/com/example/android_sms_autoscheduler/MainActivity.java
+++ app/src/main/java/com/example/android_sms_autoscheduler/MainActivity.java
@@ -16,15 +16,10 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.telephony.SmsManager;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     EditText message;
